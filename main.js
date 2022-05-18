722ecf49a49fe765d5dee87d943fa1f06e55512f
Bukarra <ogorodovreg@gmail.com>
2022-05-18 23:45:16 +0300

    18.05 - About-page-layout

---
+++ main.js
@@ -0,0 +1,11 @@
+const footerNav = document.querySelector('#footer-menu');
+
+window.addEventListener('scroll', footerAppear);
+
+function footerAppear() {
+    if (window.scrollY >= 200) {
+        footerNav.classList.add('footer__nav_visible');
+    } else {
+        footerNav.classList.remove('footer__nav_visible');
+    }
+}
